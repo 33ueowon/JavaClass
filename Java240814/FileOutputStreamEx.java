c03fe481011f4f532c1126119753662dd7705463
이여원 <127466476+33ueowon@users.noreply.github.com>
2024-08-14 11:21:29 +0900

    바이너리 파일 읽기

---
+++ Java240814/FileOutputStreamEx.java
@@ -0,0 +1,21 @@
+package Java240814;
+
+import java.io.*;
+
+public class FileOutputStreamEx {
+    public static void main(String[] args) {
+        byte b[] = {7,51,3,4,-1,24};
+        try{
+            FileOutputStream fout =
+                    new FileOutputStream("/Users/ueown/IdeaProjects/Javaclass/src/Java240814/Hangul");
+            for (int i = 0; i < b.length; i++)
+                fout.write(b[i]);
+            fout.close();
+        }catch(IOException e){
+            System.out.println("/Users/ueown/IdeaProjects/Javaclass/src/Java240814/Hangul에 저장할 수 없었습니다. 경로명을 확인해 주세요");
+
+            return;
+        }
+        System.out.println("/Users/ueown/IdeaProjects/Javaclass/src/Java240814/Hangul을 저장하였습니다.");
+    }
+}
